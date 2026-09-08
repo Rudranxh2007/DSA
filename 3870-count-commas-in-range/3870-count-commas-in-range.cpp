8f4b41b849b00df67eaaab2dda73df76448617e6
Rudransh Mishra <rudransh.2428cseai23@kiet.edu>
2026-09-08 23:43:56 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.38%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        if(n<=999){
+            return 0;
+        }
+        
+        else if(n>=1000 && n<=100000) {
+             return  (n-1000)+1;
+        }
+        
+        return 1;
+        
+    }
+};
