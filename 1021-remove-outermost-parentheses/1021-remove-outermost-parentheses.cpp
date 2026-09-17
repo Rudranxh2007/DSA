d04a4b5ace2a4631da995181af22c56ec3463096
Rudransh Mishra <rudransh.2428cseai23@kiet.edu>
2026-09-17 10:30:17 +0530

    Time: 0 ms (100%), Space: 9 MB (54.17%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        
+        string ans="";
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                count++;
+                if(count>1)ans+='(';
+            }
+            else{
+                if(count>1) ans+=')';
+                count--;
+            }
+        }
+        return ans;
+    }
+};
